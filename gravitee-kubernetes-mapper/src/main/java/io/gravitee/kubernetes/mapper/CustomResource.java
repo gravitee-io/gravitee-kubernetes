6b11b07fe3f3f4b72392e32c00ab4fe887806d87
Antoine CORDIER <acordier@chopin.local>
2022-09-07 15:27:51 +0200

    feat: add custom resource mapper module
    see https://github.com/gravitee-io/issues/issues/8143

---
+++ gravitee-kubernetes-mapper/src/main/java/io/gravitee/kubernetes/mapper/CustomResource.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.kubernetes.mapper;
+
+/**
+ * @author GraviteeSource Team
+ */
+public abstract class CustomResource<S> {
+
+    private final String apiVersion;
+    private final String kind;
+    private final ObjectMeta metadata;
+    private final S spec;
+
+    protected CustomResource(GroupVersionKind groupVersionKind, ObjectMeta metadata, S spec) {
+        this.apiVersion = groupVersionKind.group() + "/" + groupVersionKind.version();
+        this.kind = groupVersionKind.kind();
+        this.metadata = metadata;
+        this.spec = spec;
+    }
+
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public ObjectMeta getMetadata() {
+        return metadata;
+    }
+
+    public S getSpec() {
+        return spec;
+    }
+}
---
+++ gravitee-kubernetes-mapper/src/main/java/io/gravitee/kubernetes/mapper/ObjectMeta.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.kubernetes.mapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author GraviteeSource Team
+ */
+public class ObjectMeta {
+
+    private final String name;
+    private final Map<String, String> annotations = new HashMap<>();
+    private final Map<String, String> labels = new HashMap<>();
+
+    public ObjectMeta(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, String> getAnnotations() {
+        return annotations;
+    }
+
+    public Map<String, String> getLabels() {
+        return labels;
+    }
+
+    public void addLabel(String label, String value) {
+        labels.put(label, value);
+    }
+
+    public void addAnnotation(String annotation, String value) {
+        annotations.put(annotation, value);
+    }
+}
