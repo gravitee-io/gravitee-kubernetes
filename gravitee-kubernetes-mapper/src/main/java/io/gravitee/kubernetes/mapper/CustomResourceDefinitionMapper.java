5f8d0249ab432fe3b15b64fa1bebce00f73b56aa
Kamiel Ahmadpour <kamiel.ahmadpour@graviteesource.com>
2025-12-15 15:40:59 +0000

    fix: remove null values from CRD yaml file
    see https://gravitee.atlassian.net/browse/GKO-1949

--- gravitee-kubernetes-mapper/src/main/java/io/gravitee/kubernetes/mapper/CustomResourceDefinitionMapper.java
+++ gravitee-kubernetes-mapper/src/main/java/io/gravitee/kubernetes/mapper/CustomResourceDefinitionMapper.java
@@ -15,15 +15,56 @@
  */
 package io.gravitee.kubernetes.mapper;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import java.util.Iterator;
+import java.util.Map;
 
 public class CustomResourceDefinitionMapper {
 
-    ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper(new YAMLFactory());
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
 
     public <T> String toCustomResourceDefinition(CustomResource<T> customResource) throws JsonProcessingException {
+        if (customResource.getSpec() instanceof ObjectNode node) {
+            removeNulls(node);
+        }
         return objectMapper.writeValueAsString(customResource);
+    }
+
+    private void removeNulls(JsonNode node) {
+        if (node.isObject()) {
+            ObjectNode objNode = (ObjectNode) node;
+            Iterator<Map.Entry<String, JsonNode>> fields = objNode.fields();
+            while (fields.hasNext()) {
+                Map.Entry<String, JsonNode> entry = fields.next();
+                JsonNode child = entry.getValue();
+
+                if (child.isNull()) {
+                    fields.remove();
+                } else if (child.isContainerNode()) {
+                    removeNulls(child);
+                }
+            }
+        } else if (node.isArray()) {
+            ArrayNode arrNode = (ArrayNode) node;
+            for (int i = arrNode.size() - 1; i >= 0; i--) {
+                JsonNode element = arrNode.get(i);
+                if (element.isNull()) {
+                    arrNode.remove(i);
+                } else if (element.isContainerNode()) {
+                    removeNulls(element);
+                }
+            }
+        }
     }
 }
